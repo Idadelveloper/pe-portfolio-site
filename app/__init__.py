8cab71037ae829e8e72333b8a4ebf8d31bae863f
Ida Delphine <idadelm@gmail.com>
2026-07-18 19:57:42 +0100

    flask app integration tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import yaml
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
@@ -8,13 +9,17 @@ from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-                               user=os.getenv("MYSQL_USER"),
-                               password=os.getenv("MYSQL_PASSWORD"),
-                               host=os.getenv("MYSQL_HOST"),
-                               port=3306
-                    )
-print(mydb)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase("file:memory?mode=memory&cache=shared", uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                         user=os.getenv("MYSQL_USER"),
+                         password=os.getenv("MYSQL_PASSWORD"),
+                         host=os.getenv("MYSQL_HOST"),
+                         port=3306
+                         )
+    print(mydb)
 
 class TimelinePost(Model):
     name = CharField()
@@ -53,9 +58,18 @@ for page in content['nav']:
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    name = request.form.get('name')
+    if not name:
+        return "Invalid name", 400
+
+    email = request.form.get('email')
+    if not email or not re.match(r"[^@\s]+@[^@\s]+\.[^@\s]+", email):
+        return "Invalid email", 400
+
+    content = request.form.get('content')
+    if not content:
+        return "Invalid content", 400
+
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
     return model_to_dict(timeline_post)
