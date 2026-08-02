37afdc3733c9cf470ec3efc056428fc8d8080969
Ida Delphine <idadelm@gmail.com>
2026-08-02 22:39:40 +0000

    update redeply script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,7 +4,6 @@ cd ~/pe-portfolio-site || exit 1
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
 
-sudo systemctl restart myportfolio
